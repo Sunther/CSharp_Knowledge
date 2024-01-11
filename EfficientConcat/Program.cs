3bb3c6965ab0581dc2e32ddc28f06b178f1cf669
Marc Llopis <marc.llopis.sge@mango.com>
2024-01-11 10:57:00 +0100

    Added ObjectPool

--- EfficientConcat/Program.cs
+++ EfficientConcat/Program.cs
@@ -1,56 +1,90 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Extensions.ObjectPool;
+using System.Diagnostics;
 using System.Text;
 
-internal class Program
+namespace EfficientConcat
 {
-    private static void Main(string[] args)
+    public class Program
     {
-        UsingConcat();
-        UsingStringBuilder();
-    }
+        private readonly static IEnumerable<int> _naturalNumbers = Enumerable.Range(1, 10000);
 
-    private static void UsingConcat()
-    {
-        var sw = new Stopwatch();
-        var naturalNumbers = Enumerable.Range(1, 1000).ToList();
-        var naturalString = string.Empty;
-
-        sw.Start();
-        foreach (int item in naturalNumbers)
+        public static void Main()
         {
-            if (item % 2 == 0)
-            {
-                naturalString += $"{item} is Even";
-            }
-            else
-            {
-                naturalString += $"{item} is Odd";
-            }
+            UsingConcat();
+            UsingStringBuilder();
+            UsingObjectPool();
         }
-        sw.Stop();
 
-        Console.WriteLine($"{nameof(UsingConcat)} {sw.ElapsedMilliseconds} mseg");
-    }
-    private static void UsingStringBuilder()
-    {
-        var sw = new Stopwatch();
-        var naturalNumbers = Enumerable.Range(1, 1000).ToList();
-        var naturalStringBuilder = new StringBuilder();
-
-        sw.Start();
-        foreach (int item in naturalNumbers)
+        private static void UsingConcat()
         {
-            if (item % 2 == 0)
-            {
-                naturalStringBuilder.Append($"{item} is Even");
-            }
-            else
-            {
-                naturalStringBuilder.Append($"{item} is Odd");
-            }
-        }
-        sw.Stop();
+            var sw = new Stopwatch();
+            sw.Start();
+            var naturalString = string.Empty;
 
-        Console.WriteLine($"{nameof(UsingStringBuilder)} {sw.ElapsedMilliseconds} mseg");
+            foreach (int item in _naturalNumbers)
+            {
+                if (item % 2 == 0)
+                {
+                    naturalString += $"{item} is Even";
+                }
+                else
+                {
+                    naturalString += $"{item} is Odd";
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"{nameof(UsingConcat)} {sw.ElapsedMilliseconds} mseg");
+        }
+        private static void UsingStringBuilder()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            var naturalStringBuilder = new StringBuilder();
+
+            foreach (int item in _naturalNumbers)
+            {
+                if (item % 2 == 0)
+                {
+                    naturalStringBuilder.Append($"{item} is Even");
+                }
+                else
+                {
+                    naturalStringBuilder.Append($"{item} is Odd");
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"{nameof(UsingStringBuilder)} {sw.ElapsedMilliseconds} mseg");
+        }
+        /// <summary>
+        /// Microsoft.Extensions.ObjectPool is part of the ASP.NET Core infrastructure that supports keeping a group of objects in memory for reuse rather than allowing the objects to be garbage collected. All the static and instance methods in Microsoft.Extensions.ObjectPool are thread-safe.
+        ///Apps might want to use the object pool if the objects that are being managed are:
+        ///Expensive to allocate/initialize.
+        ///Represent a limited resource.
+        ///Used predictably and frequently.
+        /// </summary>
+        private static void UsingObjectPool()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            ObjectPool<StringBuilder> stringBuilderPool = new DefaultObjectPoolProvider().CreateStringBuilderPool();
+            var stringBuilder = stringBuilderPool.Get();
+
+            foreach (int item in _naturalNumbers)
+            {
+                if (item % 2 == 0)
+                {
+                    stringBuilder.Append($"{item} is Even");
+                }
+                else
+                {
+                    stringBuilder.Append($"{item} is Odd");
+                }
+            }
+            sw.Stop();
+
+            Console.WriteLine($"{nameof(UsingObjectPool)} {sw.ElapsedMilliseconds} mseg");
+        }
     }
 }
