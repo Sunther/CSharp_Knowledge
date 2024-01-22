c09d8975adcc1d1cd0d4a8777d66e8c766606e5b
Marc Llopis <marc.llopis.sge@mango.com>
2024-01-22 16:19:07 +0100

    Renamed to SmartString

---
+++ SmartString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+public class Program
+{
+    public static void Main()
+    {
+        RegexHelper.RegexExample();
+        TestRegex(@"^a\d+|b\w*");
+
+        TestDateTime(DateTime.Now, "G"); //Ctrl + Spacebar
+    }
+
+    static void TestDateTime(DateTime dateTime, [StringSyntax(StringSyntaxAttribute.DateTimeFormat)] string format) { }
+    static void TestRegex([StringSyntax(StringSyntaxAttribute.Regex)] string regex) { }
+}
+
+internal static partial class RegexHelper
+{
+    [GeneratedRegex("{(~[0-9]{1,3})+}")]
+    internal static partial Regex RegexExample();
+}
