6c0ae3f9d1dc0f2cde6b3b6c3f1a0ed298476913
Marc Llopis <marc.llopis.sge@mango.com>
2024-01-22 18:45:53 +0100

    Added retry project

--- RetryWithPolly/Program.cs
+++ RetryWithPolly/Program.cs
@@ -1,78 +1,63 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Polly;
-using Polly.Retry;
 
 public class Program
 {
+    public const string PipelineTag = "Tag";
+
+    /// <summary>
+    /// https://www.youtube.com/watch?v=PqVQFUCTzUM
+    /// </summary>
     public static void Main()
     {
         var serviceProvider = GenerateServiceProvider();
-
-        Polly();
-        ResiliencePipelineBuilder();
-        Fallback();
+        serviceProvider.GetRequiredService<IProgramService>().Run();
     }
 
     private static IServiceProvider GenerateServiceProvider()
     {
         return new ServiceCollection()
-                .AddResiliencePipeline()
-            
+                .AddResiliencePipeline<string, string>(PipelineTag, builder =>
+                    {
+                        builder.AddRetry(new()
+                        {
+                            MaxRetryAttempts = 5,
+                            BackoffType = DelayBackoffType.Constant,
+                            Delay = TimeSpan.Zero,
+                            ShouldHandle = new PredicateBuilder<string>()
+                                        .Handle<Exception>(),
+                            OnRetry = retryArguments =>
+                            {
+                                Console.WriteLine($"Current attempt: {retryArguments.AttemptNumber}\n{retryArguments.Outcome.Exception}");
+                                return ValueTask.CompletedTask;
+                            }
+                        });
+                    })
+
+                .AddScoped<IProgramService, ProgramService>()
+
                 // Build
                 .BuildServiceProvider();
     }
 
-    private static void Polly()
-    {
-        var policy = Policy
-            .Handle<Exception>()
-            .Retry(5, (ex, retryCount) =>
-            {
-                Console.WriteLine($"Current attempt: {retryCount}, {ex}");
-            });
+    //private static void Fallback()
+    //{
+    //    var pipeline = new ResiliencePipelineBuilder<string>()
+    //        .AddFallback(new FallbackStrategyOptions<string>
+    //        {
+    //            MaxRetryAttempts = 5,
+    //            BackoffType = DelayBackoffType.Constant,
+    //            Delay = TimeSpan.Zero,
+    //            ShouldHandle = new PredicateBuilder()
+    //                                .Handle<Exception>(),
+    //            OnRetry = retryArguments =>
+    //            {
+    //                Console.WriteLine($"Current attempt: {retryArguments.AttemptNumber}, {retryArguments.Outcome.Exception}");
+    //                return ValueTask.CompletedTask;
+    //            }
+    //        })
+    //        .Build();
 
-        policy.Execute(() => ProgramService.Method());
-    }
-
-    private static void ResiliencePipelineBuilder()
-    {
-        var pipeline = new ResiliencePipelineBuilder()
-            .AddRetry(new RetryStrategyOptions
-            {
-                MaxRetryAttempts = 5,
-                BackoffType = DelayBackoffType.Constant,
-                Delay = TimeSpan.Zero,
-                ShouldHandle = new PredicateBuilder()
-                                    .Handle<Exception>(),
-                OnRetry = retryArguments =>
-                {
-                    Console.WriteLine($"Current attempt: {retryArguments.AttemptNumber}, {retryArguments.Outcome.Exception}");
-                    return ValueTask.CompletedTask;
-                }
-            })
-            .Build();
-
-        pipeline.Execute(() => ProgramService.Method());
-    }
-
-    private static void Fallback()
-    {
-        var pipeline = new ResiliencePipelineBuilder()
-            .AddRetry(new RetryStrategyOptions
-            {
-                MaxRetryAttempts = 5,
-                BackoffType = DelayBackoffType.Constant,
-                Delay = TimeSpan.Zero,
-                ShouldHandle = new PredicateBuilder()
-                                    .Handle<Exception>(),
-                OnRetry = retryArguments =>
-                {
-                    Console.WriteLine($"Current attempt: {retryArguments.AttemptNumber}, {retryArguments.Outcome.Exception}");
-                    return ValueTask.CompletedTask;
-                }
-            })
-            .Build();
-
-        pipeline.Execute(() => ProgramService.Method());
-    }
+    //    pipeline.Execute(ProgramService.Method());
+    //}
 }
--- RetryWithPolly/ProgramService.cs
+++ RetryWithPolly/ProgramService.cs
@@ -1,7 +1,27 @@
-﻿public static class ProgramService
+﻿using Polly.Registry;
+
+public class ProgramService : IProgramService
 {
-    public static void Method()
+    private readonly ResiliencePipelineRegistry<string> _pipelineProvider;
+
+    public ProgramService(ResiliencePipelineRegistry<string> pipelineProvider)
+    {
+        _pipelineProvider = pipelineProvider;
+    }
+
+    public void Run()
+    {
+        var pipeline = _pipelineProvider.GetPipeline<string>(Program.PipelineTag);
+        _ = pipeline.Execute<string>(() => Method());
+    }
+
+    public static string Method()
     {
         throw new NotImplementedException();
     }
+}
+
+public interface IProgramService
+{
+    void Run();
 }
